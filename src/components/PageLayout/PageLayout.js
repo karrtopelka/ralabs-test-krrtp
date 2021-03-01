0f355d52616cbdac71d5b2fb3241bb3f93d92c42
Max Plotitsyn <karrtopelka@gmail.com>
2021-03-01 11:30:35 +0200

    deleted header and footer files, and changed them with Bar component, as they are the same..

--- src/components/PageLayout/PageLayout.js
+++ src/components/PageLayout/PageLayout.js
@@ -1,22 +1,55 @@
-import { Page, useMediaQuery } from '@geist-ui/react';
+import { Button, Page, Text, useMediaQuery } from '@geist-ui/react';
+import { Activity, Moon, Sun } from '@geist-ui/react-icons';
 import React from 'react';
+import Bar from '../Bar/Bar';
 import Content from '../Content/Content';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
 
 const PageLayout = ({ switchThemes, themeType }) => {
   const isXS = useMediaQuery('xs', { match: 'down' });
   return (
     <Page>
       <Page.Header>
-        <Header switchThemes={switchThemes} themeType={themeType} />
+        <Bar
+          left={
+            <Text h4 style={{ margin: 0 }}>
+              Karrtopelka
+            </Text>
+          }
+          right={
+            <Button
+              type={themeType === 'light' ? 'success' : 'warning'}
+              ghost
+              iconRight={themeType === 'light' ? <Moon /> : <Sun />}
+              auto
+              size="small"
+              onClick={switchThemes}
+            />
+          }
+          switchThemes={switchThemes}
+          themeType={themeType}
+        />
       </Page.Header>
       <Page.Content>
         <Content />
       </Page.Content>
       {!isXS && (
         <Page.Footer>
-          <Footer />
+          <Bar
+            left={
+              <Text p style={{ margin: 0 }}>
+                {new Date().getFullYear()}
+              </Text>
+            }
+            right={
+              <Text p style={{ margin: 0 }}>
+                Ralabs{' '}
+                <span>
+                  <Activity size={16} />
+                </span>{' '}
+                Karrtopelka
+              </Text>
+            }
+          />
         </Page.Footer>
       )}
     </Page>
